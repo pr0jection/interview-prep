05befd10a0c547b3adb8b5c6c8313b0c87f45447
pr0jection <pr0jection@localhost>
2016-11-27 17:47:03 -0500

    Dijkstra's algorithm

--- alg-graph-search.py
+++ alg-graph-search.py
@@ -35,9 +35,9 @@ class UndirectedGraph(object):
     def add_node(self, a):
         self.edges[a] = []
 
-    def add_edge(self, a, b):
-        self.edges[a].append(b)
-        self.edges[b].append(a)
+    def add_edge(self, a, b, weight=None):
+        self.edges[a].append((b, weight))
+        self.edges[b].append((a, weight))
 
     def get_edges(self, a):
         return self.edges[a]
@@ -78,3 +78,39 @@ def depth_first_search(graph, root, predicate):
                 nodes.append(adjacent)
 
     return None
+
+
+'''
+    Dijkstra's algorithm is a fairly intuitive graph search algorithm to compute
+    all shortest paths from a fixed vertex. We basically have a set of all vertices
+    and weights (initially 0 for the root, infinity for the rest), and repeatedly
+    pop the vertex with the lowest weight. We can iterate over its neighbors and
+    update their weights if lowest_weight_to_point + weight_to_neighbor is lower
+    than the currently best seen weight. Popping the vertex with the lowest weight
+    is the key to making this work, since we know (assuming non-negative weights)
+    that minimum weight is the final shortest weight to that node.
+
+    This algorithm runs in O( E + V^2 ) time, and since E = O(V^2) generally just
+    O(V^2). If we use a min heap for finding the minimum weight, we can take this
+    down to O( E + V log V ).
+'''
+def dijkstras_algorithm(graph, root):
+    nodes = set(graph.edges.keys())
+    shortest = { root: 0 }
+
+    while nodes:
+        min_node = None
+        min_weight = None
+
+        for node in shortest:
+            if node in nodes and (not min_weight or shortest[node] < min_weight):
+                min_node = node
+                min_weight = shortest[node]
+
+        nodes.remove(min_node)
+
+        for neighbor, weight in graph.get_edges(min_node):
+            if neighbor not in shortest or shortest[neighbor] > min_weight + weight:
+                shortest[neighbor] = min_weight + weight
+
+    return shortest
